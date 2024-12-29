bb715eb59dc359b8f117fd0e5538787d9c7dc4a7
r-chopra <41696066+r-chopra@users.noreply.github.com>
2024-12-29 01:51:42 -0500

    Delete comments.js

--- comments.js
+++
@@ -1,7 +0,0 @@
-// Create Web Server
-// Create a simple web server using Node.js that returns a JSON object containing the following data. 
-// The server should listen on port 3000.
-// JSON object:
-// {
-//     "name": "John Doe",
-//     "email": "
